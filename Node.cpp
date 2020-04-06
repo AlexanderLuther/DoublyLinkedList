b4c33a146edf63ebaed6975381a943606acf4b44
AlexanderLuther <41831248+AlexanderLuther@users.noreply.github.com>
2020-04-06 00:46:01 -0600

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -11,14 +11,14 @@
 /*
  * Constructores de la clase.
  */
-template <typename T>
+template <class T>
 Node<T>::Node(){
     this->value = NULL;
     this->previous = NULL;
     this->next = NULL;
 }
 
-template <typename T>
+template <class T>
 Node<T>::Node(T value){
     this->value = value;
     this->previous = NULL;
@@ -28,17 +28,17 @@ Node<T>::Node(T value){
 /*
  * Setters de la clase.
  */
-template <typename T>
+template <class T>
 void Node<T>::setValue(T value){
     this->value = value;
 }
 
-template <typename T>
+template <class T>
 void Node<T>::setPrevious(Node<T>* previous){
     this->previous = previous;
 }
 
-template <typename T>
+template <class T>
 void Node<T>::setNext(Node<T>* next){
     this->next = next;
 }
@@ -46,17 +46,17 @@ void Node<T>::setNext(Node<T>* next){
 /*
  * Getters de la clase.
  */
-template <typename T>
+template <class T>
 T Node<T>::getValue(){
     return this->value;
 }
 
-template <typename T>
+template <class T>
 Node<T>* Node<T>::getPrevious(){
     return this->previous;
 }
 
-template <typename T>
+template <class T>
 Node<T>* Node<T>::getNext(){
     return this->next;
 }
